2e4e763596018289f8224f2d065b684a1871fb6a
Kyle Yerman <kyleyerman99@gmail.com>
2025-07-02 18:40:40 -0600

    test: :white_check_mark: Add procedural testing
    Update test function to better handle errors. Update to always set fee to zero. Add algorithmic testing of many possible input combinations.

--- test/EquilibriumCalculator.ts
+++ test/EquilibriumCalculator.ts
@@ -9,6 +9,7 @@ import { findEquilibriumPoint } from "../src/utils/FindEquilibriumPoint";
 import { EquilibriumCalculator } from "../typechain-types";
 
   const PRECISION = 10n ** 18n;
+  const MAX_UINT112 = 2n ** 112n - 1n;
   
   describe("EquilibriumCalculator", function () {
     type TestParams = {
@@ -17,8 +18,6 @@ import { EquilibriumCalculator } from "../typechain-types";
         priceY?: bigint,
         concentrationX?: bigint,
         concentrationY?: bigint,
-        fee?: bigint,
-        protocolFee?: bigint,
         amountIn?: bigint
         exactIn?: boolean,
         assetZeroIsInput?: boolean
@@ -32,13 +31,21 @@ import { EquilibriumCalculator } from "../typechain-types";
         priceY: bigint,
         concentrationX: bigint,
         concentrationY: bigint,
-        fee: bigint,
-        protocolFee: bigint,
         amountIn: bigint
         exactIn: boolean,
         assetZeroIsInput: boolean
         currentReserve0: bigint,
         currentReserve1: bigint
+    }
+
+    function stringifyTestParams(params: FilledTestParams): string {
+        return JSON.stringify(params, (key, value) => {
+            if (key === 'calculator') {
+                return value.address;
+            }
+            return typeof value === 'bigint' ? value.toString() : value
+            }
+        );
     }
 
     function initParams(testParams: TestParams): FilledTestParams {
@@ -48,8 +55,6 @@ import { EquilibriumCalculator } from "../typechain-types";
             priceY: testParams.priceY ?? PRECISION,
             concentrationX: testParams.concentrationX ?? PRECISION,
             concentrationY: testParams.concentrationY ?? PRECISION,
-            fee: testParams.fee ?? 0n,
-            protocolFee: testParams.protocolFee ?? 0n,
             amountIn: testParams.amountIn ?? PRECISION / 3n,
             exactIn: testParams.exactIn ?? true,
             assetZeroIsInput: testParams.assetZeroIsInput ?? true,
@@ -72,12 +77,14 @@ import { EquilibriumCalculator } from "../typechain-types";
             priceY: params.priceY,
             concentrationX: params.concentrationX,
             concentrationY: params.concentrationY,
-            fee: params.fee,
-            protocolFee: params.protocolFee,
+            fee: 0n,
+            protocolFee: 0n,
             protocolFeeRecipient: hre.ethers.ZeroAddress
         }
 
-        const contractResult = await params.calculator.findEquilibriumPoint(
+        let contractResult;
+        try {
+        contractResult = await params.calculator.findEquilibriumPoint(
             eulerParams,
             params.amountIn,
             params.exactIn,
@@ -85,20 +92,31 @@ import { EquilibriumCalculator } from "../typechain-types";
             params.currentReserve0,
             params.currentReserve1
         );
+        } catch (e: any) {
+            contractResult = "FAILED";
+        }
 
-        const scriptResult = findEquilibriumPoint(
-            params.amountIn,
-            params.exactIn,
-            params.assetZeroIsInput,
-            params.currentReserve0,
-            params.currentReserve1,
-            params.priceX,
-            params.priceY,
-            params.concentrationX,
-            params.concentrationY
-        );
+        let scriptResult;
+        try {
+            scriptResult = findEquilibriumPoint(
+                params.amountIn,
+                params.exactIn,
+                params.assetZeroIsInput,
+                params.currentReserve0,
+                params.currentReserve1,
+                params.priceX,
+                params.priceY,
+                params.concentrationX,
+                params.concentrationY
+            );
+        } catch (e: any) {
+            scriptResult = "FAILED";
+        }
 
-        expect(scriptResult).to.equal(contractResult, "The results of the helper script and the contract do not match");
+        const errorMessage = `Params ${stringifyTestParams(params)}\nScript result: ${scriptResult}\nContract result: ${contractResult}\n`;
+        expect(typeof scriptResult).to.equal(typeof contractResult, errorMessage);
+        expect(scriptResult).to.equal(contractResult, errorMessage);
+        return scriptResult;
     }
 
     async function deployFixture() {
@@ -119,6 +137,74 @@ import { EquilibriumCalculator } from "../typechain-types";
             calculator: equilibriumCalculator,
         });
       });
+
+      it("Compares errors correctly", async function () {
+        const { equilibriumCalculator } = await loadFixture(deployFixture);
+
+        expect(await test({
+            calculator: equilibriumCalculator,
+            currentReserve1: 0n,
+            exactIn: false,
+        })).to.equal("FAILED");
+      })
+
+      it("Handles special cases", async function () {
+        const { equilibriumCalculator } = await loadFixture(deployFixture);
+
+        await test({
+            calculator: equilibriumCalculator,
+            currentReserve0: 1n,
+            priceX: 0n,
+            priceY: 1n,
+            concentrationX: 0n,
+            concentrationY: 0n,
+            amountIn: hre.ethers.MaxUint256,
+        });
+      })
+
+      it("Generally matches contract behavior", async function () {
+        const { equilibriumCalculator } = await loadFixture(deployFixture);
+
+        const uint112Points = [0n, 1n, 10n ** 3n, 10n ** 24n, MAX_UINT112];
+        const uint256Points = [0n, 1n, 10n ** 3n, 10n ** 36n, hre.ethers.MaxUint256];
+
+        // Test a broad range of possibilities for all inputs
+        // 2^2 * 4^7 = 65536 combinations
+        // Reduce to 10,000 combinations to avoid timeout
+        for (let exactIn of [true, false]) {
+            for (let assetZeroIsInput of [true, false]) {
+                for (let priceX of uint256Points) {
+                    for (let priceY of uint256Points) {
+                        for (let concentrationX of uint256Points) {
+                            for (let concentrationY of uint256Points) {
+                                for (let amountIn of uint256Points) {
+                                    for (let currentReserve0 of uint112Points) {
+                                        for (let currentReserve1 of uint112Points) {
+                                            if (Math.random() > 0.16) {
+                                                continue;
+                                            }
+                                            await test({
+                                                calculator: equilibriumCalculator,
+                                                priceX,
+                                                priceY,
+                                                concentrationX,
+                                                concentrationY,
+                                                amountIn,
+                                                exactIn,
+                                                assetZeroIsInput,
+                                                currentReserve0,
+                                                currentReserve1
+                                            });
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+      })
     });
   });
   
