8ab5ab849ca45f4eb0a3a3c76cbeaf6c69b9eea4
Kyle Yerman <kyleyerman99@gmail.com>
2025-07-03 09:50:41 -0600

    chore: :wrench: Update mocha timeout

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,9 @@ import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  mocha: {
+    timeout: 300_000 // 5 minutes
+  }
 };
 
 export default config;
